28a0097ed2e939607ba333c4d7223a4631229d90
santisfdez <sasanchezfe@unal.edu.co>
2022-06-26 14:02:37 -0500

    Update create_data_lake.py

--- src/data/create_data_lake.py
+++ src/data/create_data_lake.py
@@ -21,8 +21,52 @@ def create_data_lake():
 
 
     """
-    raise NotImplementedError("Implementar esta función")
+"""
+Función que genera una carpeta data_lake en la raiz del proyecto, con el fin de almacenar los datos requeridos
+para el desarrollo de los modelos del proyecto. 
 
+Ejemplo:
+
+>>> os.mkdir('data_lake')
+>>> root_path ='data_lake/'
+>>> list =['landing', 'raw', 'cleansed', 'business']
+>>> for items in list:
+>>>    path = os.path.join(root_path, items)
+>>>    os.mkdir(path)
+.
+    |
+    \___ data_lake/
+         |___ landing/
+         |___ raw/
+         |___ cleansed/
+         \___ business/
+      
+"""
+import os
+import sys
+
+os.mkdir('data_lake')
+
+root_path ='data_lake/'
+list =['landing', 'raw', 'cleansed', 'business']
+
+for items in list:
+    path = os.path.join(root_path, items)
+    os.mkdir(path)
+
+root_path_business = 'data_lake/business'
+list_business= ['reports', 'features', 'forecast']
+
+for items in list_business:
+    path = os.path.join(root_path_business, items)
+    os.mkdir(path)
+
+directory = 'figures'
+root_path_reports = 'data_lake/business/reports'
+path = os.path.join(root_path_reports, directory)
+os.mkdir(path)
+
+    #raise NotImplementedError("Implementar esta función")
 
 if __name__ == "__main__":
     import doctest
