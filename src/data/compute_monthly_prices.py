08402523033bb7c0cf1313c528af8489acc0585c
santisfdez <sasanchezfe@unal.edu.co>
2022-06-30 00:50:38 -0500

    Update compute_monthly_prices.py

--- src/data/compute_monthly_prices.py
+++ src/data/compute_monthly_prices.py
@@ -12,10 +12,27 @@ def compute_monthly_prices():
 
 
     """
-    raise NotImplementedError("Implementar esta función")
+    import pandas as pd
+    import os
+
+    path_origin = 'data_lake/cleansed/precios-horarios.csv'
+    final_path = 'data_lake/business/'
+    
+    day_prices = pd.read_csv('data_lake/cleansed/precios-horarios.csv', index_col=None, header=0)
+    day_prices = day_prices[['Fecha', 'Precio']]
+    day_prices['Fecha'] = pd.to_datetime(day_prices['Fecha'])
+    day_prices['Firstday']= day_prices['Fecha'].to_numpy().astype('datetime64[M]')
+    day_prices = day_prices[['Firstday', 'Precio']]
+    monthly_ave_prices = day_prices.groupby('Firstday', as_index=False).mean({'Precio': 'Precio'})
+    monthly_ave_prices.to_csv( final_path + 'precios-mensuales' + '.csv', index=None, header= True)
+    
+
+    #raise NotImplementedError("Implementar esta función")
 
 
 if __name__ == "__main__":
+
+    compute_monthly_prices()
     import doctest
 
     doctest.testmod()
