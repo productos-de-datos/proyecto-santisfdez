69d5520b1ffa4d276fa9ab20f5f6e41b410bc9a5
santisfdez <sasanchezfe@unal.edu.co>
2022-06-29 23:42:38 -0500

    Update transform_data.py

--- src/data/transform_data.py
+++ src/data/transform_data.py
@@ -10,22 +10,28 @@ def transform_data():
 
 import pandas as pd
 import xlwt
+import os
 
-start = 1995
-end = 2022
-repo_path = 'data_lake/landing'
-final_path = 'data_lake/raw/'
+landing_path = 'data_lake/landing/'
+raw_path = 'data_lake/raw/'
+files_landing_folder = os.listdir(landing_path)
+files_raw_folder = os.listdir(raw_path)
 
-for year_to_download in range (start, end):
-    try: 
-        files = pd.read_excel(repo_path + '/' + str(year_to_download) + '.xlsx')
-        files.to_csv(final_path + str(year_to_download) + '.csv', index=None, header=True)
-    except:
-        files = pd.read_excel(repo_path + '/' + str(year_to_download) + '.xls')
-        files.to_csv(final_path + str(year_to_download) + '.csv', index=None, header=True)
-
-
-
+for files in files_landing_folder:
+    if files.split('.')[-1] == 'xlsx' or files.split('.')[-1] == 'xls':
+        year_transformed = int(files.split('.')[0])
+        if year_transformed in range(1995,2000):
+            num_header = 3
+        if year_transformed in range (2000, 2018):
+            num_header = 2
+        if year_transformed in range (2018, 2022):
+            num_header = 0
+    
+        file = pd.read_excel(landing_path + files)
+        file = file.iloc[num_header:, 0:25]
+        file.columns = ['Fecha', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23']
+        file.to_csv(raw_path + str(year_transformed) + '.csv', index=None)
+        
 #raise NotImplementedError("Implementar esta función")
 
 
