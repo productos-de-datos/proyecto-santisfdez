dcc08b10ac0f1a4f4a2b41367c9e2a0ef7503417
santisfdez <sasanchezfe@unal.edu.co>
2022-07-01 21:33:16 -0500

    Update make_daily_prices_plot.py

--- src/visualization/make_daily_prices_plot.py
+++ src/visualization/make_daily_prices_plot.py
@@ -1,3 +1,6 @@
+from email.header import Header
+
+
 def make_daily_prices_plot():
     """Crea un grafico de lines que representa los precios promedios diarios.
 
@@ -7,10 +10,28 @@ def make_daily_prices_plot():
     El archivo se debe salvar en formato PNG en data_lake/business/reports/figures/daily_prices.png.
 
     """
-    raise NotImplementedError("Implementar esta función")
+    import pandas as pd
+    import matplotlib.pyplot as plt
+
+    daily_prices = pd.read_csv('data_lake/business/precios-diarios.csv', index_col=None, header=0)
+    daily_prices.plot(kind='line')
+
+    plt.plot(daily_prices['Fecha'], daily_prices['Precio'], 'b', label = 'daily average')
+    plt.title('Daily average')
+    plt.xlabel('Year')
+    plt.ylabel('Price')
+    plt.legend()
+    plt.xticks(rotation = 'vertical')
+    plt.savefig('data_lake/business/reports/figures/daily_prices.png')
+    
+    
+    #raise NotImplementedError("Implementar esta función")
 
 
 if __name__ == "__main__":
+
+    make_daily_prices_plot()
+
     import doctest
 
     doctest.testmod()
