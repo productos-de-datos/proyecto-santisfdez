79d310474d2c5e8e1d7573d1cdabc92e0227b445
santisfdez <sasanchezfe@unal.edu.co>
2022-07-02 17:11:01 -0500

    Update grader.py

--- grader.py
+++ grader.py
@@ -134,7 +134,8 @@ def test_10():
 
 def test_11():
     """Modelo creado"""
-    assert os.path.isfile("modeles/precios-diarios.pkl") is True
+    os.system("make train_daily_model")
+    assert os.path.isfile("src/models/precios-diarios.pkl") is True
 
 
 def test_12():
